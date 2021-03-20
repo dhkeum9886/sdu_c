834f2deea672ff52dd98f637d162ca9c39d0d730
dhkeum9886 <dhkeum9886@gmail.com>
2021-03-21 08:07:39 +0900

    2주차 과제

---
+++ 2w/2020620001_금동환_face.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * 파일명 : 2020620001_금동환_face
+ * 작성자 : 금동환
+ */
+
+int main() {
+    printf("************\n");
+    printf("*    @ @   *\n");
+    printf("*     ^    *\n");
+    printf("*    ~~~   *\n");
+    printf("************\n");
+    return 0;
+}
